6cca8b25994c932f941507255a7a33038db4446e
cgolobic <cgolobic@gmail.com>
2018-04-17 07:09:10 -0700

    generate perlin noise using STB implementation

--- noise.cc
+++ noise.cc
@@ -1,5 +1,9 @@
+#define STB_PERLIN_IMPLEMENTATION
+
 #include <nan.h>
 #include <iostream>
+#include <random>
+#include "stb_perlin.h"
 using namespace std;
 using namespace Nan;
 using namespace v8;
@@ -13,16 +17,21 @@ NAN_METHOD(Generate) {
   // statically declaring pixelData array here will cause it to go out of scope outside this function
   // other option is to statically declare and use Nan::CopyBuffer to create result buffer, but this
   // incurs the cost of an implicit memcpy
-  unsigned char* pixelData = new unsigned char[12]();
-  pixelData[0] = 255;
-  pixelData[3] = 255;
+  unsigned char* pixelData = new unsigned char[40000]();
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0, 1);
+  float rand;
+  for (int i = 0; i < 39996; i += 4) {
+    rand = dis(gen);
+    float res = stb_perlin_noise3(rand, rand, rand, 0, 0, 0);
+    pixelData[i] = res * 255;
+    pixelData[i + 1] = res * 255;
+    pixelData[i + 2] = res * 255;
+    pixelData[i + 3] = 255;
+  }
 
-  pixelData[5] = 255;
-  pixelData[7] = 255;
-
-  pixelData[10] = 255;
-  pixelData[11] = 255;
-  auto buf = Nan::NewBuffer((char *)pixelData, 12, del_cb, pixelData);
+  auto buf = Nan::NewBuffer((char *)pixelData, 40000, del_cb, pixelData);
   info.GetReturnValue().Set(buf.ToLocalChecked());
 }
 
